151239dfa4a4aa08b453bf0e611bea5a3ea11876
Florian Mastorakis <florian.mastorakis@hotmail.fr>
2022-02-07 16:06:02 +0100

    Exo 2 fixed

---
+++ CPP/day1/ex01/srcs/getteur_setter.cpp
@@ -0,0 +1,45 @@
+#include "../include/Phonebook.hpp"
+
+std::string    contact::get_first_name(void)
+{
+    return(this->first_name);
+}
+
+std::string    contact::get_nickname(void)  
+{
+    return(this->nickname);
+}
+
+std::string    contact::get_phone(void)
+{
+    return(this->phone_number);
+}
+
+std::string    contact::get_secret(void)
+{
+    return(this->darkest_secret);
+}
+
+void    contact::set_first_name(std::string str)
+{
+    this->first_name = str;
+    return ;
+}
+
+void    contact::set_nickname(std::string str)
+{
+    this->nickname = str;
+    return ;
+}
+
+void    contact::set_phone(std::string str)
+{
+    this->phone_number = str;
+    return ;
+}
+
+void    contact::set_secret(std::string str)
+{
+    this->darkest_secret = str;
+    return ;
+}
---
+++ CPP/day1/ex01/srcs/phonebook.cpp
@@ -0,0 +1,228 @@
+#include "../include/Phonebook.hpp"
+
+Phonebook::Phonebook(void)
+{
+    std::cout << "Construction called" << std::endl;    
+}
+
+Phonebook::~Phonebook(void)
+{
+    std::cout << "Destructor called" << std::endl;
+    return ;
+}
+
+void    Phonebook::init_contact(int nbr_contact)
+{
+    this->_nb_contact= nbr_contact;
+}
+
+int     ft_strlen(std::string str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+void    ft_putstr(std::string str)
+{
+    int i;
+    int on_right;
+
+    i = 0;
+    on_right = 0;
+    if (ft_strlen(str) >= 0 && ft_strlen(str) <= 10)
+    {
+        on_right = 10 - ft_strlen(str);
+        while(on_right > 0)
+        {
+            std::cout << ' ';
+            on_right--;
+        }
+        i = 0;
+        while (i < ft_strlen(str))
+        {
+            std::cout << str[i];
+            i++;
+        }
+    }
+    else if (ft_strlen(str) > 9)
+    {
+        i = 0;
+        while (i < 9)
+        {
+            std::cout << str[i];
+            i++;
+        }
+        std::cout << '.';
+    }
+
+}
+
+void    Phonebook::display(int i)
+{
+    (void)i;
+    std::cout << MAGENTA;
+    std::cout << "|-------------------------------------------|" << std::endl;
+	std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
+	std::cout << "|-------------------------------------------|" << std::endl;
+    std::cout << "|";
+    std::cout << NC;
+    ft_putstr(this->_contact[i].get_first_name());
+    std::cout << MAGENTA;
+    std::cout << "|";
+    std::cout << NC;
+    ft_putstr(this->_contact[i].get_nickname());
+    std::cout << MAGENTA;
+    std::cout << "|";
+    std::cout << NC;
+    ft_putstr(this->_contact[i].get_phone());
+     std::cout << MAGENTA;
+    std::cout << "|";
+    std::cout << NC;
+   ft_putstr(this->_contact[i].get_secret());
+    std::cout << MAGENTA;
+    std::cout << "|\n";
+    std::cout << NC;
+
+}
+
+void    Phonebook::display_all(int i)
+{
+    (void)i;
+    std::cout << MAGENTA;
+    std::cout << "|-------------------------------------------|" << std::endl;
+	std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
+	std::cout << "|-------------------------------------------|" << std::endl;
+    std::cout << NC;
+    i = 0;
+    while(i < 8)
+    {
+        std::cout << MAGENTA;
+        std::cout << "|";
+        std::cout << NC;
+        ft_putstr(this->_contact[i].get_first_name());
+        std::cout << MAGENTA;
+        std::cout << "|";
+        std::cout << NC;
+        ft_putstr(this->_contact[i].get_nickname());
+        std::cout << MAGENTA;
+        std::cout << "|";
+        std::cout << NC;
+        ft_putstr(this->_contact[i].get_phone());
+        std::cout << MAGENTA;
+        std::cout << "|";
+        std::cout << NC;
+        ft_putstr(this->_contact[i].get_secret());
+        std::cout << MAGENTA;
+        std::cout << "|\n";
+        std::cout << NC;
+        i++;
+    }
+
+}
+void    Phonebook::new_contact(int i)
+{
+    int j;
+    std::string buff;
+
+    j = 0;
+    (void)i;
+    while (j < 5)
+    {
+        if (j == 0)
+        {
+            std::cout << "Please write down the first name" << std::endl;
+            std::cin >> buff;
+            this->_contact[i].set_first_name(buff);
+
+        }
+        else if(j == 1)
+        {
+            std::cout << "Please write down the nickname" << std::endl;
+            std::cin >> buff;
+            this->_contact[i].set_nickname(buff);
+        }
+        else if (j == 2)
+        {
+            std::cout << "Please write down the phone number" << std::endl;
+            std::cin >> buff;
+            this->_contact[i].set_phone(buff);
+        }
+        else if (j == 3)
+        {
+            std::cout << "Please write down your darkest secret " << std::endl;
+            std::cin >> buff;
+            this->_contact[i].set_secret(buff);
+        }
+        j++;
+    }
+}
+
+int    Phonebook::look_for_contact(std::string i)
+{
+    int j;
+    
+    j = 0;
+    while (i[j] == ' ')
+        j++;
+    j = i[0] - 48;
+    if (j < 0 || j > 7)
+    {
+        std::cout << "Bad information\n";
+        return (-1);
+    }
+    else
+    {
+        return (j);
+    }
+}
+
+int main(int ac, char **av)
+{
+    Phonebook repertoire;
+    std::string buff;
+    int cursor;
+
+    (void)av;
+    cursor = 0;
+    if (ac == 1)
+    {
+        while (1)
+        {
+            std::cout << "\n\nWaiting for command" << std::endl;
+            std::cout << "You can choose between ADD / SEARCH / EXIT/ SHOW :" << std::endl;
+            std::cout << "=>";
+             std::cin >> buff;
+            if (buff == "ADD\0")
+            {
+                std::cout << "\nYou have selected ADD .. \n" << std::endl;
+                if (cursor == 8)
+                    cursor = 0;
+                repertoire.new_contact(cursor);
+                cursor++;
+            }
+            else if (buff == "SEARCH\0")
+            {
+                std::cout << "\nYou have selected SEARCH .. \n" << std::endl;
+                 std::cin >> buff;
+                 if(repertoire.look_for_contact(buff) > -1)
+                    repertoire.display(repertoire.look_for_contact(buff));
+            }
+            else if (buff == "SHOW\0")
+            {
+                repertoire.display_all(0);
+            }
+            else if (buff == "EXIT\0")
+                break ;
+            else
+            {
+                std::cout << RED << "Wrong command if you wanna exit please press [CTRL] + [C] or EXIT" << std::endl;
+                std::cout << NC;
+            }
+        }
+    }
+    return (0);
+}
