ee39418873aeaf16158172b59575b56f8d526fe3
Florian Mastorakis <florian.mastorakis@hotmail.fr>
2022-03-09 18:18:57 +0100

    day 07 finsihs

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: florianmastorakis <florianmastorakis@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 10:57:41 by florianmast       #+#    #+#             */
-/*   Updated: 2022/03/09 14:58:22 by florianmast      ###   ########.fr       */
+/*   Updated: 2022/03/09 17:39:53 by florianmast      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,119 @@
 #include <string>
 
 
+template <typename T>
 class Array
 {
 private:
+    T               *_tab;
+    unsigned int    _size;
+    
+    //In this case the exception has to be declared in private otherwise is is defined in the result type of the class (error message)
+    struct limitException : public std::exception
+    {
+        const char * what () const throw()
+        {
+            return ("Not a good index");
+        }
+    };
 
 public:
-    Array();
-    ~Array();
-    Array (const Array &src);
-    Array &operator=(const Array &src); 
 
+///////*| Constructor |*///////
+    Array() : _tab(new T[0]), _size(0)
+    {
+        ;
+    }
+    Array(unsigned int n) 
+    {
+        this->_tab = new T[n];
+        this->_size = n;
+        unsigned int i;
+        i = 0;
+        while (i < this->_size)
+       {
+           this->_tab[i] =  T();
+           i++;
+       }
+    }
 
+    /*| Destructor |*/
+    ~Array()
+    {
+        if (this->_tab)
+            delete [] this->_tab;
+    }
+//////*| Copy constructor |*/////
+    /* Deep copy is taken into consideration by renewing a new pointer|*/
+    Array (const Array &src)
+    {
+        this->_size = src._size;
+        this->_tab = new T[src._size];
+        unsigned int i;
+
+        i = 0;
+        while (i < src._size)
+        {
+            this->_tab[i] = src._tab[i];
+            i++;
+        }
+           
+    }
+//////*| Assignation copy |*//////
+    Array &operator=(const Array &src)
+    {
+        if (this == &src)
+            return (*this);
+        else
+        {
+            unsigned int i ;
+
+            i = 0;
+            delete [] this->_tab;
+            this->_size = src._size;
+            this->_tab = new T[src._size];
+            while (i < this->_size)
+            {
+                this->_tab[i] = src._tab[i];
+                i++;
+            }
+            return (*this);
+        }
+    }
+//////*| Return the size |*/////
+    unsigned int size(void) const
+    {
+        return (this->_size);
+    }
+//////*| Overload the operator [] |*/////
+    T   &operator[](unsigned int index)
+    {
+        if (index >= this->_size)
+            throw limitException();
+        else
+            return (this->_tab[index]);
+    }
+    
+    const T   &operator[](unsigned int index) const
+    {
+        if (index >= this->_size)
+            throw limitException();
+        else
+            return (this->_tab[index]);
+    }
+    void    display(void)
+    {
+        unsigned int i;
+
+        i = 0;
+        while (i < this->_size)
+        {
+            std::cout << this->_tab[i] << " - " ;
+            i++;
+        }
+        std::cout << std::endl;
+    }
 };
+
 
 #endif
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -6,15 +6,122 @@
 /*   By: florianmastorakis <florianmastorakis@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 13:29:36 by florianmast       #+#    #+#             */
-/*   Updated: 2022/03/09 14:56:28 by florianmast      ###   ########.fr       */
+/*   Updated: 2022/03/09 17:53:04 by florianmast      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Array.hpp"
 
+int main(int, char**)
+{  
+    std::cout << "TEST WITH INT" << std::endl;
+    std::cout << "=============" << std::endl;
+    Array<int> numbers(10);
+    int i;
+    
+    i = 0;
+    //Conseil : Essayez de compiler int * a = new int(); puis affichez *a. Suite au conseil l'init  est: this->_tab[i] =  T();
+    while (i < 10)
+    {
+        std::cout << numbers[i] << " - ";
+        i++;
+    }
+    std::cout << std::endl;
+    std::cout << "=============" << std::endl; 
 
-int main() 
-{
-     
-    return 0;
-}   
+ 
+    std::cout << "\nTEST WITH STRING" << std::endl;
+    std::cout << "=============" << std::endl;
+    Array<std::string> str(10);
+    i = 0;
+    while (i < 10)
+    {
+        std::cout << str[i] << " - ";
+        i++;
+    }
+    std::cout << std::endl; 
+    std::cout << "=============" << std::endl;
+
+    
+    std::cout << "\nTEST FOR ASSIGNATION" << std::endl;
+    std::cout << "=============" << std::endl;
+    Array<int> nb1(10);
+    Array<int> nb2;
+    i = 0;
+    while (i < 10)
+    {
+        nb1[i] = i;
+        i++;
+    }
+    while (i < 10)
+    {
+        std::cout << nb1[i] << " - ";
+        i++;
+    }
+    std::cout << "try to display nb2: \n" ;
+    nb2.display();
+    nb2 = nb1;
+    std::cout << "Assignation  done: \n" ;
+    nb1.display();
+    nb2.display();
+    
+    std::cout << std::endl; 
+    std::cout << "=============" << std::endl;
+
+    std::cout << "\nTEST FOR COPY " << std::endl;
+    std::cout << "=============" << std::endl;
+    Array<int> assi(10);
+    i = 0;
+    while (i < 10)
+    {
+        assi[i] = i;
+        i++;
+    }
+    Array<int> assi1(assi);
+    std::cout << "try to display assi1: \n" ;
+    assi1.display();
+    assi1 = assi;
+    std::cout << " Copy done: \n" ;
+    assi.display();
+    assi1.display();
+    
+    std::cout << std::endl; 
+    std::cout << "=============" << std::endl;
+
+    std::cout << "\nTEST FOR EMPTY OR NOT GOOD SIZE" << std::endl;
+    std::cout << "=============" << std::endl;
+    Array<int> wrong;
+    try
+    {
+        wrong[1] = 1;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "=============" << std::endl;
+    std::cout << "\nTEST FOR OPERATOR OVERLOAD" << std::endl;
+    std::cout << "Good using with good index: " << std::endl;
+     try
+    {
+        std::cout << assi1[5] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "Bad using with bad index: " << std::endl;
+    try
+    {
+        std::cout << assi1[41] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+    
+ 
+}
